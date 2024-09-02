2bd6d0afff8c98727a9d5416cbb3d4b018ca4e3c
JaiminGOD <32418702+JaiminBrahmbhatt@users.noreply.github.com>
2024-09-01 22:30:32 -0500

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
         {slides.map((slide, index) => (
           <div
             key={index}
-            className={` w-full h-full transition-opacity duration-1000 ${
+            className={`w-full h-full transition-opacity duration-1000 ${
               index === currentSlide ? "opacity-100" : "opacity-0"
             }`}
           >
@@ -38,17 +38,19 @@ const Hero = () => {
               src={slide.image}
               alt={slide.title}
               layout="fill"
-              objectFit="cover"             
+              objectFit="cover"
             />
           </div>
         ))}
-        <div className="absolute bg-secondary bottom-[100px] left-1/2 transform -translate-x-1/2 flex space-x-2 rounded-full py-3 px-7">
+
+        {/* Slide Indicator */}
+        <div className="absolute bg-green-500 bottom-[100px] left-1/2 transform -translate-x-1/2 flex space-x-2 rounded-full py-2 px-5">
           {slides.map((_, index) => (
             <button
               key={index}
               onClick={() => goToSlide(index)}
-              className={`w-4 h-4 rounded-full mx-1 ${
-                index === currentSlide ? 'bg-blue-500' : 'bg-gray-400'
+              className={`w-3 h-3 rounded-full mx-1 ${
+                index === currentSlide ? 'bg-blue-500' : 'bg-gray-200'
               }`}
               aria-label={`Go to slide ${index + 1}`}
             />
