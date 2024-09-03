72fdb76ce845b787ad7c7a68eb942c9f57c0c708
JaiminGOD <32418702+JaiminBrahmbhatt@users.noreply.github.com>
2024-09-02 21:04:50 -0500

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
                 <a
                   href={`#${link.id}`}
                   onClick={(e) => handleScroll(e, link.id)}
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
+                  className="block py-2 px-3 text-gray-900 text-lg font-semibold rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
                 >
                   {link.title}
                 </a>
