97ecf1babdf5599bc7abfb998351f7e63055d168
JaiminGOD <32418702+JaiminBrahmbhatt@users.noreply.github.com>
2024-09-02 22:47:29 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,10 @@
-import { Inter } from 'next/font/google'
+import { Outfit } from 'next/font/google'
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import ScrollToTopButton from '../components/ScrollToTopButton';
 import Topbar from "@/components/Topbar";
 
-const inter = Inter({
+const outfit = Outfit({
   subsets: ['latin'],
 })
 
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={outfit.className}>
         <Topbar/>
         <Navbar />
         <main className="overflow-hidden text-xl relative">{children}</main>
